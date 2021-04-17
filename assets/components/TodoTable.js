d73ca1dcbb6c4abd12c30da08c1aef9adfda0a79
slobodanperusinovic <slobodan.perusinovic@gmail.com>
2021-04-18 00:06:17 +0200

    createTodo method

--- assets/components/TodoTable.js
+++ assets/components/TodoTable.js
@@ -1,12 +1,49 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {TodoContext} from "../contexts/TodoContext";
+import {Table, TableHead, TableRow, TableCell, TableBody, IconButton, TextField} from "@material-ui/core";
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 function TodoTable() {
     const context = useContext(TodoContext);
+    const [addTodo, setAddTodo] = useState('');
+
     return (
-        <div>{context.todos.map(todo => (
-            <div key={todo.name}>{todo.name}</div>
-        ))}</div>
+        <form onSubmit={(event) => {
+            context.createTodo(event, {name: addTodo})
+        }}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Task</TableCell>
+                        <TableCell align="right">Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+                        <TableCell>
+                            <TextField value={addTodo} onChange={(event) => {setAddTodo(event.target.value)}} label="New task" fullWidth={true} />
+                        </TableCell>
+                        <TableCell align="right">
+                            <IconButton type="submit">
+                                <AddIcon/>
+                            </IconButton>
+                        </TableCell>
+                    </TableRow>
+                    {context.todos.slice().reverse().map((todo, index) => (
+                        <TableRow key={'todo '+index}>
+                            <TableCell>{todo.name}</TableCell>
+                            <TableCell align="right">
+                                <IconButton><EditIcon/></IconButton>
+                                <IconButton><DeleteIcon/></IconButton>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </form>
     );
 
 }
